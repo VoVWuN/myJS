571860793786120b095bc66bd47056367b8fa89e
VoVFuN <vovfun@gmail.com>
2022-06-01 22:18:34 +0200

    lesson-3

--- js/main.js
+++ js/main.js
@@ -1,105 +1,84 @@
-class ProductList {
-    constructor(container = ".products") {
+class ProductsList {
+  constructor(container = '.products'){
       this.container = container;
       this.goods = [];
-      this._fetchProducts(); //рекомендация, чтобы метод был вызван в текущем классе
-      this.render(); //вывод товаров на страницу
-      this.totalProductPrice = 0;
-    }
-    _fetchProducts() {
+      this.allProducts = [];
+      this._fetchProducts();
+  }
+  
+  _fetchProducts() {
       this.goods = [
-        { id: 1, title: "Notebook", price: 2000, img: "images/notebook" },
-        { id: 2, title: "Mouse", price: 20, img: "images/mouse" },
-        { id: 3, title: "Keyboard", price: 200, img: "images/keyboard" },
-        { id: 4, title: "Gamepad", price: 50, img: "images/gamepad"},
+          {id: 1, title: 'Notebook', price: 2000},
+          {id: 2, title: 'Mouse', price: 20},
+          {id: 3, title: 'Keyboard', price: 200},
+          {id: 4, title: 'Gamepad', price: 50},
       ];
-    }
-  
-    render() {
+  }
+  render() {
       const block = document.querySelector(this.container);
-      for (let product of this.goods) {
-        const item = new ProductItem(product);
-        block.insertAdjacentHTML("beforeend", item.render());
-        // block.innerHTML += item.render();
+      for(let product of this.goods){
+          const productObj = new ProductItem(product);
+          this.allProducts.push(productObj);
+          block.insertAdjacentHTML('beforeend',productObj.render())
       }
-    }
+  }
+  getSum() {
+       let s = 0;
+//         this.goods.forEach(item=>{
+//             s += item.price;
+//         })
+      
+      
+//        for(let product of this.goods){
+//            s += product.price;
+//        }
+      //reduce используется для последовательной обработки каждого элемента массива с сохранением промежуточного результата.
+      let res = this.allProducts.reduce((s, item) => s + item.price,0);
+      alert(res);
+  }    
+}
+
+
+class ProductItem {
+constructor(product, img = 'https://via.placeholder.com/200x150'){
+  this.title = product.title;
+  this.price = product.price;
+  this.id = product.id;
+  this.img = img;
   
-    getTotalProoductsPrice() {
-      for (let i = 0; i < this.goods.length; i++) {
-        this.totalProductPrice += this.goods[i].price;
-      }
-      return this.totalProductPrice;
-      // console.log(
-      //   `Суммарная стоимость всех товаров равна ${this.totalProductPrice}`
-      // );
-    }
+}
+
+render() {
+   return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <h3>${this.title}</h3>
+              <p>${this.price}</p>
+              <button class="buy-btn">Купить</button>
+          </div>`
+}
+}
+
+let list = new ProductsList();
+list.render();
+list.getSum();
+
+class Basket {
+  addGood() {
+
+  }
+  removeGood() {
+
+  }
+  changeGood() {
+
   }
   
-  class ProductItem {
-    constructor(product) {
-      this.title = product.title;
-      this.id = product.id;
-      this.price = product.price;
-      this.img = product.img;
-    }
-    render() {
-      return `<div class="product-item">
-                  <img src=${this.img}>
-                  <h3>${this.title}</h3>
-                  <p>${this.price}</p>
-                  <button class="buy-btn">Купить</button>
-              </div>`;
-    }
+  render(){
+      
   }
-  
-  class CartList {
-    constructor(container = ".cart") {
-      this.container = container;
-      this.goodsInCart = [];
-    }
-  
-    renderCart() {
-      this.container.insertAdjacentHTML("beforeend", `<ul class"cartList"></ul>`);
-    }
-  
-    addItemToCartList(cartItem) {
-      this.cartItem = cartItem;
-      this.count = 0;
-    }
-  
-    renderProductsIncart() {}
-  
-    renderNewProductsInCart() {}
-  
-    removeProductFromCart() {}
-  
-    getTotalCartCount() {}
-  
-    getTotalCartPrice() {}
-  }
-  
-  class CartItem extends ProductItem {
-    constructor(product) {
-      super(product);
-      this.count = 0;
-    }
-  
-    createCartItem() {
-      const cartItem = new CartItem(product);
-      return cartItem;
-    }
-  
-    render() {
-      return `<li class="cart__item">
-                <img src=${this.img}>
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-              </li>`;
-    }
-  }
-  
-  let list = new ProductList();
-  
-  list.getTotalProoductsPrice();
-  
-  const cart = new CartList();
+}
+
+class ElemBasket {
+  render(){}
+
+}
